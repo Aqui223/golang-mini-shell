3e307a93f4084d3ddc253955bc6fa21690303a9d
Aqui <aquiren@proton.mail>
2026-03-01 01:17:46 +0500

    Added support for argv

--- main.go
+++ main.go
@@ -1,16 +1,33 @@
 package main
 
 import (
+    "bufio"
     "syscall"
     "fmt"
+    "strings"
+    "os"
 )
 
 func main() {
-    var command string = "none"
+    var command string
+    var execable_fp string
+    var fields []string
     var arg_vars []string
     var env_vars []string
+
+    scanner := bufio.NewScanner(os.Stdin)
+
     for {
-        fmt.Scanln(&command)
-        syscall.Exec(command, arg_vars, env_vars)
+        fmt.Print("~$ ")
+        if !scanner.Scan() {
+            break
+        }
+        command = scanner.Text()
+        fields = strings.Fields(command)
+        arg_vars = fields[0:]
+        execable_fp = fields[0]
+        fmt.Println(arg_vars)
+
+        syscall.Exec(execable_fp, arg_vars, env_vars)
     }
 }
