35ddd4bc2d9eb1911912c1900b2789113dcee41c
Aqui <aquiren@proton.mail>
2026-03-24 13:00:23 +0500

    More accurate autocomplete

--- main.go
+++ main.go
@@ -55,12 +55,13 @@ func main() {
     var fields []string
     var arg_vars []string
     var env_vars []string
+    var bash_history []string
     var status syscall.WaitStatus
 
     //env_vars = append(env_vars, "A=1")
     file, err := os.Open("envs")
     if err == nil {
-        fmt.Println("Loading envs file")
+        fmt.Println("Loading envs")
     
         defer file.Close()
 
@@ -74,8 +75,22 @@ func main() {
         }
     }
 
+    file, err = os.Open("bash_history")
+    if err == nil {
+        fmt.Println("Loading bash_history")
+        defer file.Close()
+        scanner := bufio.NewScanner(file)
+        for scanner.Scan() {
+            bash_history = append(bash_history, scanner.Text())
+        }
+    }
+
     exec.Command("stty", "-F", "/dev/tty", "cbreak", "min", "1").Run()
     exec.Command("stty", "-F", "-echo").Run()
+
+    var score float64
+    var best_match int = 0
+    var best_match_score float64 = 0
 
     for {
         command = ""
@@ -92,8 +107,35 @@ func main() {
             } else if b[0] == 127 && len(command) != 0 {
                 command = command[:len(command)-1]
                 fmt.Print("\r$ "+command+"\033[0J")
+            } else if b[0] == 9 {
+                command = bash_history[best_match]
+                fmt.Print("\r$ "+command+"\033[0J")
             } else {
                 command += string([]byte{b[0]})
+                fmt.Print("\r$ "+command+"\033[0J")
+                best_match = -1
+                best_match_score = 0
+
+                for history_line_number, history_line := range bash_history {
+                    score = 0
+                    if len(history_line) <= len(command) {
+                        continue
+                    }
+                    for i, r := range command { // later use binary search
+                        if history_line[i] == string(r)[0] {
+                            score += 1 / float64(i+1)
+                        }
+                    }
+                    if score > best_match_score {
+                        best_match_score = score
+                        best_match = history_line_number
+                    }
+                }
+                if best_match != -1 {
+                    fmt.Print("\x1b[2m")
+                    fmt.Print(bash_history[best_match][len(command):])
+                    fmt.Print("\x1b[0m")
+                }
             }
         }
         fmt.Print("\x1b[2D  \n")
